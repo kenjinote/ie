d855bf1e13dd4d950878ea47ddb8eaeea2393c66
kenjinote <2605401+kenjinote@users.noreply.github.com>
2022-03-09 22:20:34 +0900

    プロジェクト ファイルを追加します。

---
+++ Source.cpp
@@ -0,0 +1,17 @@
+﻿#include <windows.h>
+#import <shdocvw.dll>
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPreInst, LPWSTR pCmdLine, int nCmdShow)
+{
+	HRESULT hr = CoInitialize(NULL);
+	if (SUCCEEDED(hr)) {
+		SHDocVw::IWebBrowser2Ptr pIE;
+		hr = pIE.CreateInstance(__uuidof(SHDocVw::InternetExplorer));
+		if (SUCCEEDED(hr)) {
+			pIE->PutVisible(VARIANT_TRUE);
+			SetForegroundWindow((HWND)pIE->GetHWND());
+		}
+	}
+	CoUninitialize();
+	return 0;
+}
